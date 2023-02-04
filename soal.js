92c4af6d92ebaac7b0aa04245a38d4c2182069fc
ulrmdhn <auliarama965@gmail.com>
2023-02-04 10:28:30 +0700

    first commit

---
+++ soal.js
@@ -0,0 +1,236 @@
+// 1
+
+// const arr = [5,6,1,8,2,9,6,0,7,4];
+// arr.splice(-1)
+
+// console.log(arr)
+
+// 2
+
+// const arrNum = [5,6,1,8,2,9,6,0,7,4];
+
+// arrNum.push(3);
+
+// console.log(arrNum)
+
+// 3
+
+// const arrDel = [5,6,1,8,2,9,6,0,7,4];
+
+// arrDel.shift();
+
+// console.log(arrDel);
+
+// 4
+
+// const arrAdd = [5,6,1,8,2,9,6,0,7,4];
+
+// arrAdd.unshift(0);
+
+// console.log(arrAdd);
+
+// 5
+
+// const angka_1 = [5,6,1,8,2,9,6,0,7,4];
+// const angka_2 = [8,7,6];
+
+// const angka = [...angka_1, ...angka_2]
+// console.log(angka)
+
+// 6
+
+// const sortAsc = [5,6,1,8,2,9,6,0,7,4];
+// sortAsc.sort()
+
+// console.log(sortAsc)
+
+// 7
+
+// const sortDsc = [5,6,1,8,2,9,6,0,7,4];
+// sortDsc.sort().reverse();
+
+// console.log('sortDsc', sortDsc)
+
+// --------------------------------
+
+// 8
+
+// const str = ["apple", "banana", "cherry"];
+// const fruit = str.toString()
+
+// console.log(fruit)
+
+// 9
+
+// const str = "apple,banana,cherry";
+// const fruit = str.split(',');
+// console.log(fruit);
+
+// 10
+
+// const text = "halo";
+// const hasil = text.toUpperCase();
+
+// console.log(hasil)
+
+// 11
+
+// const upper = "HALO";
+// const lower = upper.toLocaleLowerCase();
+
+// console.log('lower:', lower)
+
+// 12
+
+// const hitung = "halo";
+// const wS = hitung.replace(/\s/g, "")
+// const panjang = wS.length;
+
+// console.log(panjang)
+
+
+// 13
+
+// const replace ="halo semua";
+// const txtReplace = replace.replace('semua', 'dunia');
+// console.log(txtReplace);
+
+// 14
+
+// const input = "halo";
+// const output = input.charAt(2);
+
+// console.log(output);
+
+// 15
+
+// const firstWord = "halo";
+// const secondWord = "semua";
+
+// console.log(firstWord + secondWord);
+
+// 16
+
+// const kata = "coba coba";
+// const hasil = kata.slice(1, 6);
+
+// console.log(hasil)
+
+// 17
+
+// const kata = "javascript";
+// const hasil = kata.charAt(9)
+// console.log(hasil)
+
+// 18
+
+// const kata = "halo semua";
+// const hasil = kata[0].toUpperCase() + kata.slice(1);
+// console.log(hasil);
+
+// 19
+
+// let obj = {
+//     name: "John Doe",
+//     age: 30,
+//     job: "Developer",
+//   }
+
+//   obj.address = "Jkt"
+
+
+// console.log(obj);
+
+// 20
+
+// let obj = {
+//     name: "John Doe",
+//     age: 30,
+//     job: "Developer",
+//   }
+
+//   delete obj['job'];
+
+//   console.log(obj);
+
+// 21
+
+// let obj = {
+//     name: "John Doe",
+//     age: 30,
+//     job: "Developer",
+//   }
+
+// console.log(Object.hasOwn(obj, 'age'));
+
+// 22
+
+// let obj = {
+//     name: "John Doe",
+//     age: 30,
+//     job: "Developer",
+//   }
+
+//   let keys = Object.keys(obj)
+//   console.log(keys);
+
+// 23
+
+// let obj = {
+//     name: "John Doe",
+//     age: 30,
+//     job: "Developer",
+//   }
+
+// let keys = Object.keys(obj);
+// for (let i = 0; i < keys.length; i++) {
+//     let val = obj[keys[i]];
+//     console.log(val)
+// }
+
+
+// 24
+
+// let obj1 = {
+//     name: "John Doe",
+//     age: 30,
+// }
+
+// let obj2 = {
+//     job: "Developer",
+//     address: "New York",
+// }
+
+// const user = Object.assign(obj1, obj2);
+
+// console.log(user);
+
+// 25
+
+// let obj1 = {
+//     name: "John Doe",
+//     age: 30,
+//   }
+
+// obj1.age = '43';
+
+// console.log(obj1);
+
+// 26
+
+// let obj = {
+//     name: "John Doe",
+//     age: 30,
+//     job: "Developer",
+// }
+
+// obj.skills = ["js", "java"]
+
+// console.log(obj)
+
+
+
+
+
+
+
---
+++ soal1.md
@@ -0,0 +1,63 @@
+Diberikan array [5,6,1,8,2,9,6,0,7,4]
+
+1. Hapus item terakhir dari array. Hasilnya [5,6,1,8,2,9,6,0,7]
+2. Tambahkan angka 3 di akhir array. Hasilnya [5,6,1,8,2,9,6,0,7,4,3]
+3. Hapus item pertama dari array. Hasilnya [6,1,8,2,9,6,0,7,4]
+4. Tambah angka 0 di awal array. Hasilnya [0,5,6,1,8,2,9,6,0,7,4]
+5. Gabungkan array diatas dengan [8,7,5]. Hasilnya [5,6,1,8,2,9,6,0,7,4,8,7,5]
+6. Sort array diatas dari kecil ke besar (ascending)
+7. Sort array diatas dari besar ke kecil (descending)
+
+---
+
+8. Ubah array ["apple", "banana", "cherry"] menjadi apple, banana, cherry
+9. Ubah kembali hasil nomor 8 menjadi ["apple", "banana", "cherry"]
+10. Ubah string "halo" menjadi "HALO"
+11. Ubah string "HALO" menjadi "halo"
+12. Hitung jumlah kata "halo semua" tanpa spasi
+13. Ganti kata "halo semua" menjadi "halo dunia"
+14. Ambil karakter ke 3 dari "halo" 
+15. Gabungkan kata "halo" dan "semua"
+16. Tampilkan karakter ke 2 sampai 5 dari "coba coba javascript"
+17. Tampilkan karakter terakhir dari "javascript"
+18. Ubah kalimat "halo semua" menjadi "Halo Semua"
+
+---
+
+```js
+let obj = {
+  name: "John Doe",
+  age: 30,
+  job: "Developer",
+}
+```
+
+19. Tambah properti "address" dengan value "Jkt"
+20. Hapus properti "job" dari obj
+21. Cek apakah obj punya properti age
+22. Ambil semua properti dari obj
+23. Ambil semua value dari obj
+24. Gabungkan 2 object ini
+
+```js
+let obj1 = {
+  name: "John Doe",
+  age: 30,
+}
+let obj2 = {
+  job: "Developer",
+  address: "New York",
+}
+```
+
+25. Ubah nilai age dari obj menjadi 43
+26. Tambahkan skill js dan java ke obj. Hasilnya
+
+```js
+let obj = {
+  name: "John Doe",
+  age: 30,
+  job: "Developer",
+  skills: ["js", "java"],
+}
+```
